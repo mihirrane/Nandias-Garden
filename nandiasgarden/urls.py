4bf4eafce23e3f00e96e6a5f2717cfe35744691b
mihirrane <ranemihir45@gmail.com>
2020-12-24 20:53:52 -0600

    Plain HTML style website

--- nandiasgarden/urls.py
+++ nandiasgarden/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pizza import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.Home, name = 'home'),
+    path('order', views.order, name = 'order')
 ]
